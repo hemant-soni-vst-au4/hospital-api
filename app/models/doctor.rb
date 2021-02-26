2ba2ad39e11c0b5e33f705c2d99dbf79168db61d
Hemant soni <sonihemant4741@gmail.com>
2021-02-26 20:56:50 +0530

    doctor model validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,7 @@
 class Doctor < ApplicationRecord
+    validate :name, presence: true
+    VALID_EMAIL_ADDRESS = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validate :email, presence: true, uniqueness: true,
+                      format: { with: VALID_EMAIL_ADDRESS },
+                      length: { minimum: 6, maximum: 100}
 end
