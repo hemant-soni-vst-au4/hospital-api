69a3e2683af519a6a02e3cddc865ec9684b9ee8c
Hemant soni <sonihemant4741@gmail.com>
2021-03-01 20:08:52 +0530

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :patients do
+        resources :appointments
+      end
+      post "patient/login", to:"patients#login"
+      get "patient/auto_login", to:"patients#auto_login"
+      resources :doctors do
+        resources :schedules
+        resources :appointments
+      end
+      post "doctor/login", to:"doctors#login"
+      get "doctor/auto_login", to:"doctors#auto_login"
+    end
+  end
 end
