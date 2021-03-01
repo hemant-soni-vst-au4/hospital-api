a9cf00e79e139ba992ab818c114fcb74f859d4e1
Hemant soni <sonihemant4741@gmail.com>
2021-03-01 23:18:17 +0530

    test for patient model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,9 +4,8 @@ class Doctor < ApplicationRecord
   has_many :schedules
   has_many :appointments, through: :schedules
 
-  validate :name, presence: true
   VALID_EMAIL_ADDRESS = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validate :email, presence: true, uniqueness: true,
+  validates :email, presence: true, uniqueness: true,
                    format: { with: VALID_EMAIL_ADDRESS },
                    length: { minimum: 6, maximum: 100 }
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,7 +3,7 @@ class Patient < ApplicationRecord
 
   has_many :appointments
   VALID_EMAIL_ADDRESS = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validate :email, presence: true, uniqueness: true,
+  validates :email, presence: true, uniqueness: true,
                    format: { with: VALID_EMAIL_ADDRESS },
                    length: { minimum: 6, maximum: 100 }
 end
