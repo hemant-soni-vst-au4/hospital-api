6e8f42a7011d0b0f4d5df5e5610f2bd911f6d918
Hemant soni <sonihemant4741@gmail.com>
2021-03-01 19:26:21 +0530

    column change in schedule

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_135322) do
+ActiveRecord::Schema.define(version: 2021_03_01_135529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,14 +42,13 @@ ActiveRecord::Schema.define(version: 2021_03_01_135322) do
   end
 
   create_table "schedules", force: :cascade do |t|
-    t.date "start_date"
-    t.string "start_time"
-    t.date "end_date"
-    t.string "end_time"
+    t.date "date"
+    t.string "time"
     t.integer "duration"
     t.bigint "doctor_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "available", default: true
     t.index ["doctor_id"], name: "index_schedules_on_doctor_id"
   end
 
